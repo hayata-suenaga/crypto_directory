29ff1071f9deec6fd0dc3d36c16f0eb61d7141ca
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-05-05 08:06:22 -0400

    Type detailed exchange api response

--- types.d.ts
+++ types.d.ts
@@ -1,14 +1,40 @@
 interface Exchange {
-    id: string;
-    name: string;
-    year_established: number;
-    country: string;
-    description: string;
-    url: string;
-    image: string;
-    has_trading_incentive: boolean;
-    trust_score: number;
-    trust_score_rank: number;
-    trade_volume_24h_btc: number;
-    trade_volume_24h_btc_normalized: number;
+  id: string;
+  name: string;
+  year_established: number;
+  country: string;
+  description: string;
+  url: string;
+  image: string;
+  has_trading_incentive: boolean;
+  trust_score: number;
+  trust_score_rank: number;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+}
+
+interface DetailedExchange {
+  name: string;
+  year_established: number;
+  country: string | null;
+  description: string;
+  url: string;
+  image: string;
+  facebook_url: string;
+  reddit_url: string;
+  telegram_url: string;
+  slack_url: string;
+  other_url_1: string;
+  other_url_2: string;
+  twitter_handle: string;
+  has_trading_incentive: boolean;
+  centralized: boolean;
+  public_notice: string;
+  alert_notice: string;
+  trust_score: number;
+  trust_score_rank: number;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+  tickers: any[];
+  status_updates: any[];
 }
