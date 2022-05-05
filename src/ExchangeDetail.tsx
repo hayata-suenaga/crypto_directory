5c581bfcceaa0ff9c327a088882e539ee18c9058
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-05-04 21:36:52 -0400

    Add back to dashboard button

--- src/ExchangeDetail.tsx
+++ src/ExchangeDetail.tsx
@@ -1,8 +1,14 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ExchangeDetail = () => {
   const params = useParams();
-  return <p>{params.id}</p>;
+  return (
+    <main>
+      <Link to="/">
+        <button>Back to Dashboard</button>
+      </Link>
+    </main>
+  );
 };
 
 export default ExchangeDetail;
