7fcf07a6e24deb470cd2778c519992c9d9d0daa7
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-05-05 08:06:44 -0400

    Create exchange detail page

--- src/ExchangeDetail.tsx
+++ src/ExchangeDetail.tsx
@@ -1,6 +1,5 @@
 import { Link, useParams } from "react-router-dom";
 import { useApi } from "./hooks";
-import ExchangeCard from "./ExchangeCard";
 import React from "react";
 
 const endpoint = "https://api.coingecko.com/api/v3/exchanges/";
@@ -10,7 +9,6 @@ const ExchangeDetail = () => {
   const { data: exchange, loading, errored } = useApi(endpoint + params.id);
 
   if (loading) return <div>Loading...</div>;
-
   if (errored)
     return (
       <div>Something went wrong... Please refresh your browser again :)</div>
@@ -18,7 +16,7 @@ const ExchangeDetail = () => {
 
   return (
     <main>
-      <ExchangeCard exchange={exchange} />
+      <DetailCard exchange={exchange} />
       <Link to="/">
         <button>Back to Dashboard</button>
       </Link>
@@ -27,3 +25,46 @@ const ExchangeDetail = () => {
 };
 
 export default ExchangeDetail;
+
+interface DetailCardProps {
+  exchange: DetailedExchange;
+}
+
+// name, country, trust rank, logo, year of establishment, social media links, description, and a back-to-main-page button.
+const DetailCard = ({ exchange }: DetailCardProps) => {
+  const {
+    image,
+    name,
+    country,
+    url,
+    trust_score_rank,
+    year_established,
+    description,
+  } = exchange;
+  return (
+    <article>
+      <img src={image} alt="exchange logo" />
+      <h2>{name}</h2>
+      <p>{`country: ${country ? country : "N/A"}`}</p>
+      <p>{`url: ${url}`}</p>
+      <p>{`rank: ${trust_score_rank}`}</p>
+      <p>{`Established in: ${year_established}`}</p>
+      <p>{description ? "Description not available" : description}</p>
+      <SocialMediaList exchange={exchange} />
+    </article>
+  );
+};
+
+const SocialMediaList = ({ exchange }: { exchange: DetailedExchange }) => {
+  const { facebook_url, reddit_url, telegram_url, slack_url, twitter_handle } =
+    exchange;
+  return (
+    <ul>
+      {facebook_url && <li>{`Facebook: ${facebook_url}`}</li>}
+      {reddit_url && <li>{`Reddit: ${reddit_url}`}</li>}
+      {telegram_url && <li>{`Telegram: ${telegram_url}`}</li>}
+      {slack_url && <li>{`Slack: ${slack_url}`}</li>}
+      {twitter_handle && <li>{`Twitter: ${twitter_handle}`}</li>}
+    </ul>
+  );
+};
